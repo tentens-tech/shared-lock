c316e1cc1d5ce94db6efdc058f25aebc5e852ace
Vladyslav Papidokha <vladyslav.papidokha@gmail.com>
2025-02-26 16:07:18 +0200

    Made return vars named for the storage interface

--- internal/infrastructure/storage/storage.go
+++ internal/infrastructure/storage/storage.go
@@ -3,7 +3,7 @@ package storage
 import "context"
 
 type Connection interface {
-	CheckLeasePresence(ctx context.Context, key string) (bool, error)
-	CreateLease(ctx context.Context, key string, leaseTTL int64, data []byte) (string, int64, error)
+	CheckLeasePresence(ctx context.Context, key string) (isPresent bool, err error)
+	CreateLease(ctx context.Context, key string, leaseTTL int64, data []byte) (leaseStatus string, leaseID int64, err error)
 	KeepLeaseOnce(ctx context.Context, leaseID int64) error
 }
