96e9f7b1264ec4406cb4b819f725c73cf8fe7ad7
Vladyslav Papidokha <vladyslav.papidokha@gmail.com>
2025-02-26 16:38:28 +0200

    Updated field naming

--- internal/application/command/leaseManagement/lease.go
+++ internal/application/command/leaseManagement/lease.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Lease struct {
-	Key     string            `json:"key"`
-	Value   string            `json:"value"`
-	Labels  map[string]string `json:"labels"`
-	Created time.Time         `json:"timestamp"`
+	Key       string            `json:"key"`
+	Value     string            `json:"value"`
+	Labels    map[string]string `json:"labels"`
+	CreatedAt time.Time         `json:"timestamp"`
 }
