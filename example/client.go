c77d7bfc41a7891278f7d4c4b4c67e3e106168a1
Vladyslav Papidokha <vladyslav.papidokha@gmail.com>
2025-03-25 13:35:00 +0200

    Fixed client to work properly

--- example/client.go
+++ example/client.go
@@ -13,7 +13,7 @@ const (
 	baseURL           = "http://localhost:8080"
 	leaseEndpoint     = "/lease"
 	keepaliveEndpoint = "/keepalive"
-	leaseTTL          = "2s"            // Base time to live for a lease
+	leaseTTL          = "3s"            // Base time to live for a lease
 	retryInterval     = 2 * time.Second // Time to wait before retrying to obtain a lease
 	keepaliveInterval = 2 * time.Second // Time interval to send keepalive requests
 )
@@ -114,7 +114,7 @@ func sendKeepalive(leaseID string) error {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusNoContent {
+	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("unexpected response status: %v, body: %v", resp.Status, resp.Body)
 	}
 
