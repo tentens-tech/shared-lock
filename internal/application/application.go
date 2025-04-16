2d9a78e80b891dd2c94240108cd1a0c06fdf77b9
Vladyslav Papidokha <vladyslav.papidokha@gmail.com>
2025-04-16 17:12:56 +0300

    Fixed bug on cache work

--- internal/application/application.go
+++ internal/application/application.go
@@ -32,16 +32,27 @@ func (a *Application) CreateLease(
 	leaseTTL time.Duration,
 	lease leasemanagement.Lease,
 ) (leaseStatus string, leaseID int64, err error) {
-	leaseStatus, leaseID, err = leasemanagement.CreateLease(a.ctx, a.storageConnection, leaseTTL, lease)
-	if err != nil {
-		log.Errorf("%v", err)
-		metrics.LeaseOperations.WithLabelValues(metrics.LeaseOperationGet, "error").Inc()
+	defer metrics.LeaseOperations.WithLabelValues(metrics.LeaseOperationGet, leaseStatus).Inc()
 
-		return "", 0, err
+	cachedLeaseID := a.checkCreatedLeasePresenceInCache(lease.Key)
+	if cachedLeaseID == 0 {
+		leaseStatus, leaseID, err = leasemanagement.CreateLease(a.ctx, a.storageConnection, leaseTTL, lease)
+		if err != nil {
+			log.Errorf("%v", err)
+			metrics.LeaseOperations.WithLabelValues(metrics.LeaseOperationGet, "error").Inc()
+
+			return "", 0, err
+		}
+
+		a.addLeaseToCache(lease.Key, leaseStatus, leaseID, leaseTTL)
+
+		return leaseStatus, leaseID, nil
 	}
 
-	metrics.LeaseOperations.WithLabelValues(metrics.LeaseOperationGet, leaseStatus).Inc()
-	return leaseStatus, leaseID, nil
+	log.Debugf("Lease already created with ID: %d", cachedLeaseID)
+	leaseStatus = storage.StatusAccepted
+
+	return leaseStatus, cachedLeaseID, nil
 }
 
 func (a *Application) ReviveLease(leaseID int64) error {
@@ -56,32 +67,20 @@ func (a *Application) ReviveLease(leaseID int64) error {
 	return nil
 }
 
-func (a *Application) CheckLeasePresenceInCache(key string) bool {
-	if a.leaseCache == nil {
-		return false
-	}
-
-	if _, exists := a.leaseCache.Get(key); exists {
-		return true
-	}
-
-	return false
-}
-
-func (a *Application) GetLeaseFromCache(key string) (string, int64) {
+func (a *Application) checkCreatedLeasePresenceInCache(key string) int64 {
 	if cachedValue, exists := a.leaseCache.Get(key); exists {
 		if cachedLease, ok := cachedValue.(cache.LeaseCacheRecord); ok {
-			log.Debugf("Cache hit for lease key: %v", key)
-			metrics.CacheOperations.WithLabelValues(metrics.LeaseOperationGet, "success").Inc()
-
-			return cachedLease.Status, cachedLease.ID
+			if cachedLease.Status == "created" {
+				log.Debugf("Cache hit for lease key: %v", key)
+				return cachedLease.ID
+			}
 		}
 	}
 
-	return "", 0
+	return 0
 }
 
-func (a *Application) AddLeaseToCache(key string, status string, id int64, ttl time.Duration) {
+func (a *Application) addLeaseToCache(key string, status string, id int64, ttl time.Duration) {
 	if a.leaseCache == nil {
 		return
 	}
